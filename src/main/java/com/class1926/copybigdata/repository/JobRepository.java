8b87beffcd371535f5affc358ae71f1066c3be74
Administrator <m18956371967@163.com>
2019-06-12 16:56:51 +0800

    version 2.0

--- src/main/java/com/class1926/copybigdata/repository/JobRepository.java
+++ src/main/java/com/class1926/copybigdata/repository/JobRepository.java
@@ -16,7 +16,7 @@ public interface JobRepository extends JpaRepository<Job, Long> {
      * @param type:参数表示所要查找的行业，不带参数表示所有行业一起查询
      * @return
      */
-    @Query(value = "select address as city,AVG(salary) as avgByCity,SUM(hiring) as countByCity,AVG(experience) as experience,SUM(salary) as budget FROM job_java where if(?1 != '',type =?,1=1) group by address", nativeQuery = true)
+    @Query(value = "select address as city,AVG(salary) as avgByCity,SUM(hiring) as countByCity,AVG(experience) as experience,SUM(salary) as budget FROM job_java where if(?1 != '',type =?1,1=1) group by address", nativeQuery = true)
     List<Object[]> findGroupByCity(String type);
 
     /**
@@ -24,7 +24,7 @@ public interface JobRepository extends JpaRepository<Job, Long> {
      * @param type:参数表示所要查找的行业，不带参数表示所有行业一起查询
      * @return
      */
-    @Query(value = "select province,AVG(salary) as avgByProvince,SUM(hiring) as countByProvince,AVG(experience) as experience,SUM(salary) as budget FROM job_java where if(?1 != '',type =?,1=1)  group by province", nativeQuery = true)
+    @Query(value = "select province,AVG(salary) as avgByProvince,SUM(hiring) as countByProvince,AVG(experience) as experience,SUM(salary) as budget FROM job_java where if(?1 != '',type =?1,1=1)  group by province", nativeQuery = true)
     List<Object[]> findGroupByProvince(String type);
 
     /**
@@ -32,7 +32,7 @@ public interface JobRepository extends JpaRepository<Job, Long> {
      * @param type:参数表示所要查找的行业，不带参数表示所有行业一起查询
      * @return
      */
-    @Query(value = "SELECT education,count(hiring) FROM job_java where if(?1 != '',type =?,1=1) GROUP BY education",nativeQuery = true)
+    @Query(value = "SELECT education,count(hiring) FROM job_java where if(?1 != '',type =?1,1=1) GROUP BY education",nativeQuery = true)
     List<Object[]> findCountGroupByEducation(String type);
 
 
@@ -42,10 +42,10 @@ public interface JobRepository extends JpaRepository<Job, Long> {
      * @return
      */
     @Query(value = "SELECT u.address,u.uisalary,j.javasalary,b.bigdatasalary,c.cloudsalary " +
-            "FROM(select address,AVG(salary) as uisalary FROM job_ui GROUP BY address)u," +
-            "(select address,AVG(salary) as javasalary FROM job_java GROUP BY address)j," +
-            "(select address,AVG(salary) as bigdatasalary FROM job_bigdata GROUP BY address)b," +
-            "(select address,AVG(salary) as cloudsalary FROM job_cloud GROUP BY address)c " +
+            "FROM(select address,AVG(salary) as uisalary FROM job_java where type='ui' GROUP BY address)u," +
+            "(select address,AVG(salary) as javasalary FROM job_java where type='java' GROUP BY address)j," +
+            "(select address,AVG(salary) as bigdatasalary FROM job_java where type='bigdata' GROUP BY address)b," +
+            "(select address,AVG(salary) as cloudsalary FROM job_java where type='cloud' GROUP BY address)c " +
             "WHERE u.address=j.address and u.address=b.address and u.address=c.address", nativeQuery = true)
     List<Object[]> findAvg();
 }
